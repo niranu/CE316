2a3c39652bd044f044c72c8a66cf9a10d9f68539
AverageJavaConsumer <106677781+AverageJavaConsumer@users.noreply.github.com>
2024-04-28 23:51:21 +0300

    Fixed an issue with SDK

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,10 @@
 module com.example.ce316 {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
             
         requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires net.synedra.validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-            requires com.almasb.fxgl.all;
-    
+                        requires org.kordamp.bootstrapfx.core;
+            
     opens com.example.ce316 to javafx.fxml;
     exports com.example.ce316;
 }
