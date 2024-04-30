fc97c5d124983b7e5e73054484bbe3b42edf428e
Sytoknia <atahanaydin989@gmail.com>
2024-04-30 22:06:04 +0300

    fxml
    Pencere büyümesi küçülmesi eklendi,başlangıçta pencerenin normal boyutla açılması sağlandı

--- src/main/java/com/example/ce316/HelloApplication.java
+++ src/main/java/com/example/ce316/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("AIE");
         stage.setScene(scene);
         stage.show();
