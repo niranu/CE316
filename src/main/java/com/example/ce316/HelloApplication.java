33681b64b34a4c49fde56ae7a5d0c3a1e4e0113e
nesibePekcakar <nesibe.pekcakar@gmail.com>
2024-05-01 18:54:11 +0300

    Scene Changing added

--- src/main/java/com/example/ce316/HelloApplication.java
+++ src/main/java/com/example/ce316/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("AIE");
+        stage.setTitle("Integrated Assignment Environment");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/ce316/HelloController.java
+++ src/main/java/com/example/ce316/HelloController.java
@@ -1,14 +1,100 @@
 package com.example.ce316;
 
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private Button createNewProjectButton;
+    @FXML
+    private Button openExistingProjectButton;
+    @FXML
+    private Button assignmentReportsButton;
+    @FXML
+    private Button BackButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private URL location;
+    @FXML
+    private StackPane mainStackPane;
+
+
+
+    @FXML
+    void initialize() {
+
     }
+
+    //Scene Changing when clicked on tha main window
+
+
+
+   private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void setOpenExistingProjectButton(ActionEvent e) throws IOException {
+
+       //Pane view = new FXMLLoader(HelloApplication.class.getResource("OpenExisting.fxml")).load();
+        root = FXMLLoader.load(getClass().getResource("OpenExisting.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+   }
+
+    /* public void setCreateNewProjectButton(ActionEvent e) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("NewProject.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }*/
+
+    public void setAssignmentReportsButton(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("ReportAssignment.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void setBackButton(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
+
+
+
 }
