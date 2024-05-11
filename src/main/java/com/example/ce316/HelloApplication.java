a2f433d8c4b42f07033b93eea13881f526ceea9a
AverageJavaConsumer <106677781+AverageJavaConsumer@users.noreply.github.com>
2024-05-11 22:34:04 +0300

    Logo image and app-main needed updates for logo

--- src/main/java/com/example/ce316/HelloApplication.java
+++ src/main/java/com/example/ce316/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.ce316;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +11,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        // Load FXML
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+
+        // Set the icon
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/Image/Logo.png"));
+        stage.getIcons().add(applicationIcon);
+
+        // Setup the stage
         stage.setTitle("Integrated Assignment Environment");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
